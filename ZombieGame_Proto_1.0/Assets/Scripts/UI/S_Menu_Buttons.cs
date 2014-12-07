f3c7371aba08928e89a75eafc2f89057f7523817
Mechanolith <j.kuskoff@hotmail.com>
2014-12-07 12:24:01 +1000

    Main Menu & Weapon Pickups
    Brought in the basic main menu and the modelled pickups for the weapons
    (NOT Ammo).

---
+++ ZombieGame_Proto_1.0/Assets/Scripts/UI/S_Menu_Buttons.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class S_Menu_Buttons : MonoBehaviour {
+	public Color defaultColor, hoverColor;
+
+	public enum ButtonKinds{
+		start,
+		controls,
+		credits,
+		menu
+	}
+
+	public ButtonKinds buttonType;
+
+	void Start () {
+		renderer.material.color = defaultColor;
+	}
+
+	void OnMouseOver(){
+		renderer.material.color = hoverColor;
+	}
+	
+	void OnMouseExit(){
+		renderer.material.color = defaultColor;
+	}
+
+	void OnMouseDown(){
+		switch(buttonType){
+			case ButtonKinds.start:
+			Application.LoadLevel("Level_NonRandom");
+			break;
+
+			case ButtonKinds.controls:
+			Camera.main.transform.position = new Vector3(20,0,0);
+			break;
+
+			case ButtonKinds.credits:
+			Camera.main.transform.position = new Vector3(40,0,0);
+			break;
+
+			case ButtonKinds.menu:
+			Camera.main.transform.position = Vector3.zero;
+			break;
+
+			default:
+			break;
+		}
+	}
+}
