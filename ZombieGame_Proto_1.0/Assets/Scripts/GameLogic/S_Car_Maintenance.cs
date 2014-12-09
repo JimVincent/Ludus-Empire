4240b0ada98a51dc59507630271aefde991f31f7
Huxley Dowling <hdowling87@hotmail.com>
2014-12-09 12:09:13 +1000

    extra script

---
+++ ZombieGame_Proto_1.0/Assets/Scripts/GameLogic/S_Car_Maintenance.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class S_Car_Maintenance : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
